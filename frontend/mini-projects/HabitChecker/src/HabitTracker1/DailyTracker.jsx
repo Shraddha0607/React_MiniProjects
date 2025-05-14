2e8bc15360b0339f3410e4a467063fa1db305d47
shraddhagaur0607 <shraddhagaurjob0607@gmail.com>
2025-05-14 15:33:45 +0530

    Structure the folders.

--- frontend/mini-projects/HabitChecker/src/HabitTracker1/DailyTracker.jsx
+++ frontend/mini-projects/HabitChecker/src/HabitTracker1/DailyTracker.jsx
@@ -3,25 +3,28 @@ import { useEffect, useState } from "react";
 function DailyTracker({ date }) {
     const [checkedHabits, setCheckedHabits] = useState([]);
     const [habits, setHabits] = useState([]);
+    const [percentDone, setPercentageDone] = useState(0);
 
     useEffect(() => {
         const allHabits = localStorage.getItem("habits");
-        console.log("by fetching from local ", allHabits);
         setHabits(JSON.parse(allHabits));
     }, []);
 
     useEffect(() => {
-        console.log("date for date wise habit fetching ", date);
         const checkHabitsInLocal = localStorage.getItem(date);
-        console.log("all data for date wise habit fetching " ,checkHabitsInLocal);
         const data = JSON.parse(checkHabitsInLocal);
         {(data === null) ? setCheckedHabits([]) : setCheckedHabits(data) }
         
     }, [date]);
 
+    useEffect(() => {
+        const percent = ((checkedHabits.length > 0 && habits.length > 0) ? Math.ceil(checkedHabits.length *100  / habits.length) : 0);
+        setPercentageDone(percent);
+    }, [checkedHabits, habits]);
+    
+
     function onSaveHandler() {
         localStorage.setItem(date, JSON.stringify(checkedHabits));
-        console.log(`save clicked for ${date} tracker`);
     }
 
     const handleCheckboxChange = (habitId) => (event) => {
@@ -39,6 +42,7 @@ function DailyTracker({ date }) {
     return (
         <div>
             <h2> Habits checklist for  {date}</h2>
+            <progress value={percentDone} max="100" />
             {habits.map((habit) => (
                 <p key={habit.id}>
                     <input type="checkbox" value={habit.name}
